8e5155004149b6c9e521285db8854ac89e8b2337
kadirtikil <kadirtikil@gmail.com>
2024-04-16 12:52:51 +0200

    these changes shouldnt be used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MetricsexplainedComponent } from './metricsexplained/metricsexplained.component';
+import { MetricselectionComponent } from './metricselection/metricselection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,
-    MetricsexplainedComponent
+    MetricsexplainedComponent, MetricselectionComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
