d702329f5344b9644c1a85df81927276f74d7324
Kadir Tikil <kadirtikil@gmail.com>
2024-04-16 16:03:14 +0200

    Frontend looks somewhat ok has to be finished but lookin good tho

--- src/app/metricselection/metricselection.component.ts
+++ src/app/metricselection/metricselection.component.ts
@@ -18,4 +18,24 @@ export class MetricselectionComponent {
     "Geringe Latenz",
     "Fehlertoleranz",
   ]
+
+  // selected um zu markieren ob ein button geklickt wurde
+  selected: boolean = false;
+
+  // Anzahl buttons um dann den geklickten button zu markieren
+  buttonState: boolean[] = [];
+  constructor() {
+    const buttonamount = this.listValues.length;
+
+    for(let i = 0; i < buttonamount; i++) {
+      this.buttonState.push(false);
+    }
+  }
+
+
+  toggleSelected(index: number) {
+    event?.preventDefault();
+    this.buttonState[index] = !this.buttonState[index];
+    console.log(this.buttonState);
+  }
 }
