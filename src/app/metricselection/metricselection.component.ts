9ca96d128af681047438ab0bb5440d356108688e
kadirtikil <kadirtikil@gmail.com>
2024-04-16 14:04:42 +0200

    some changes regarding buttons in the selection

--- src/app/metricselection/metricselection.component.ts
+++ src/app/metricselection/metricselection.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-metricselection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './metricselection.component.html',
   styleUrl: './metricselection.component.scss'
 })
 export class MetricselectionComponent {
-
+  listValues = ["Verteiltes Rechnen",
+    "Nebenläufigkeit",
+    "Wartbar- und Erweiterbarkeit",
+    "Datenintensiv",
+    "Testbarkeit",
+    "Sicherheitsanforderungen",
+    "Geringe Latenz",
+    "Fehlertoleranz",
+  ]
 }
