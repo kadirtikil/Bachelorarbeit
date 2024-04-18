c591de57c8c314149665a8dba2b9def5799ae7da
Kadir Tikil <kadirtikil@gmail.com>
2024-04-18 17:48:05 +0200

    nebenläufigkeit. paar front end zeug sachen müssen gemachtdings werden. danach kann schon doe verbindung zur api in c#.net ausgebaut. die noch entwickelt werden muss...... spew

--- src/app/concurrency/concurrency.component.ts
+++ src/app/concurrency/concurrency.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialogContent } from '@angular/material/dialog';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-concurrency',
@@ -13,19 +13,18 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 export class ConcurrencyComponent {
 
   profileForm = new FormGroup({
-    name: new FormControl(''),
-    duration: new FormControl('')
+    name: new FormControl('', [Validators.minLength(4), Validators.required, Validators.maxLength(13)]),
+    duration: new FormControl('', [Validators.minLength(1), Validators.required, Validators.maxLength(2)])
   });
 
   tasks: { name: String; duration: String; }[] = [];
-
 
   addTask() {
     event?.preventDefault();
     const name = this.profileForm.value.name;
     const duration = this.profileForm.value.duration;
     
-    // Check if name is not undefined before assigning
+    // Check if name is not undefined or null before assigning
     if (name !== undefined && duration !== undefined && name !== null && duration !== null) {
         this.tasks.push({ name: name.toString(), duration: duration.toString() });
     }
