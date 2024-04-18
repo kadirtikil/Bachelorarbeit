fd53a321477079f850a1389a0fc9cf23c54cea79
kadirtikil <kadirtikil@gmail.com>
2024-04-18 23:38:40 +0200

    pushing the backend

---
+++ Controllers/EGFuPSController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EGFuPSBackendCS.Controllers
+{
+    public class EGFuPSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("taskScheduler")]
+        public IActionResult taskScheduler()
+        {
+            return Ok(new { message = "Connection successful" });
+        }
+
+
+    }
+}
