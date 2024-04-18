a45bb6aaa583b4c516bcbb57fa27bdd5a020a185
kadirtikil <kadirtikil@gmail.com>
2024-04-18 23:05:41 +0200

    time to set up the backend

--- src/app/concurrency/concurrency.component.ts
+++ src/app/concurrency/concurrency.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialogContent } from '@angular/material/dialog';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -6,7 +7,7 @@ import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angula
 @Component({
   selector: 'app-concurrency',
   standalone: true,
-  imports: [MatDialogModule, MatDialogContent, ReactiveFormsModule, ],
+  imports: [MatDialogModule, MatDialogContent, ReactiveFormsModule, CommonModule ],
   templateUrl: './concurrency.component.html',
   styleUrl: './concurrency.component.scss'
 })
@@ -19,17 +20,22 @@ export class ConcurrencyComponent {
 
   tasks: { name: String; duration: String; }[] = [];
 
-  addTask() {
+  addTask(): void {
     event?.preventDefault();
     const name = this.profileForm.value.name;
     const duration = this.profileForm.value.duration;
     
     // Check if name is not undefined or null before assigning
-    if (name !== undefined && duration !== undefined && name !== null && duration !== null) {
+    if (name !== undefined && duration !== undefined && name !== null && duration !== null && this.tasks.length < 3) {
         this.tasks.push({ name: name.toString(), duration: duration.toString() });
     }
 
-    console.log(JSON.stringify(this.tasks));
+    console.log(this.tasks.length);
+  }
+
+  submitTasks(): void {
+    event?.preventDefault();
+    // http stuff to send the data to the backend for the algorithm
   }
 
 }
