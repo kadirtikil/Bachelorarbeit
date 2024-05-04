2ca818bb40a73a8fc6bc7cd78a946feea87729a5
kadirtikil <kadirtikil@gmail.com>
2024-05-05 01:15:41 +0200

    only lines are missing. got the coords now.

--- src/app/metricselection/metricselection.component.ts
+++ src/app/metricselection/metricselection.component.ts
@@ -1,10 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 
-import { CdkDrag } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragEnd } from '@angular/cdk/drag-drop';
 
 import { FunctionalConceptsExplainationComponent } from '../functional-concepts-explaination/functional-concepts-explaination.component';
 
@@ -19,6 +19,15 @@ export class MetricselectionComponent implements OnInit{
 
   constructor(@Inject(MatDialog) private dialog: MatDialog) { }
 
+  @ViewChild('posTester') draggable!: ElementRef;
+
+
+  // Function to handle drag end event find better event and log the coords in a schedule on ngoninit. the function inside is aight, just something else instead of cdkfragend
+  dragEnded(event: CdkDragEnd) {
+    // Get the position of the dragged element
+    const position = this.draggable.nativeElement.getBoundingClientRect();
+    console.log('Position:', position);
+  }
 
   dialogConfig: MatDialogConfig = {
     maxHeight: '90vh',
@@ -50,5 +59,7 @@ export class MetricselectionComponent implements OnInit{
   // ngoninit to periodically update the line
   ngOnInit(): void {
       //setInterval(()=>console.log(this.positionA), 1000);
+      //setInterval(() => console.log(document.getElementById("posTester")?.getBoundingClientRect()), 1000)
+      console.log(this.draggable)
   }
 }
