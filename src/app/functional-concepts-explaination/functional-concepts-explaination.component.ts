7504fadbe705d01c38fab187ecbd56f56dc8d2d0
kadirtikil <kadirtikil@gmail.com>
2024-05-04 13:25:47 +0200

    reduce a whoooooole lot of stuff, next i want to make the frontend draggable. and then connect them with lines. might add particles next.

--- src/app/functional-concepts-explaination/functional-concepts-explaination.component.ts
+++ src/app/functional-concepts-explaination/functional-concepts-explaination.component.ts
@@ -7,28 +7,90 @@ import { MarkdownModule } from 'ngx-markdown';
 
 import { RetrieveFileForMDService } from '../retrieve-file-for-md.service';
 
+import { ConcurrencyComponent } from '../concurrency/concurrency.component';
+import { DataintensiveComponent } from '../dataintensive/dataintensive.component';
+import { VertRechComponent } from '../vert-rech/vert-rech.component';
+
 
 @Component({
   selector: 'app-functional-concepts-explaination',
   standalone: true,
-  imports: [MarkdownModule, CommonModule,  ],
+  imports: [MarkdownModule, CommonModule, ConcurrencyComponent, DataintensiveComponent, 
+    VertRechComponent,
+  ],
   templateUrl: './functional-concepts-explaination.component.html',
   styleUrl: './functional-concepts-explaination.component.scss'
 })
 export class FunctionalConceptsExplainationComponent implements OnInit{
-  @Input() headline: any;
+  @Input() headline: string= "";
   @Input() description: any;
+  
+  // Boolean value to Render headline if component is supposed to have a headline
+  hasHeadline: boolean = false;
+
+  //Boolean value to Render Markdown if it has one. isloaded is for synchronizing purposes waiting for markdown from backend, then rendering it.
+  // because ngoninit is sometimed to fast.
   isLoaded: boolean = false;
+  hasMarkdown: boolean = false;
+  
+  // Boolean for div that contains the application. 
+  hasApplication: boolean = false;
+
+  // temporary placeholer, cause i cant adress a payload from server as json immediatly. have to save it here first then it works.
   temp: any = "";
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private retrieveFile: RetrieveFileForMDService) {}
 
+
   async ngOnInit() {
     try {
-    this.headline = this.data.headline;
-    this.temp = await this.retrieveFile.getTextFile("mdtext").toPromise();
-    this.description = JSON.parse(this.temp["message"]);
-    this.isLoaded=true;
+      this.headline = this.data.headline;
+
+      const mappedOptions: { [key: string]: string } = {
+        "Pure Functions": "pf",
+        "Higher Order Functions": "hof",
+        "Immutability": "imm",
+        "Monads": "mon",
+        "Pattern Matching": "pm",
+        "Lazy Evaluation": "le",
+        "Sicherheitsanforderungen": "sec",
+        "Wartbar-, Erweiterbar- und Testbarkeit": "wet",
+      };
+
+      const mappedOptionswithApplications: {[key: string]: string} = {
+        "Verteiltes Rechnen": "ds",
+        "Concurrency": "conc",
+        "Datenintensiv": "di",
+        "Message Passing": "mp",
+      }
+
+      const headlineString = this.headline.toString();
+      
+      if(this.headline in mappedOptions){
+        const fileId = mappedOptions[headlineString as keyof typeof mappedOptions];
+        this.temp = await this.retrieveFile.getTextFile(fileId).toPromise();
+        this.description = JSON.parse(this.temp["message"]);
+        // Setting the booleans. everything is loadet here time to render.
+        // No application. only markdown here.
+        this.hasHeadline= true;
+        this.hasMarkdown=true;
+        this.isLoaded=true;
+      } else if(this.headline in mappedOptionswithApplications){
+        const fileId = mappedOptionswithApplications[headlineString as keyof typeof mappedOptionswithApplications];
+        this.temp = await this.retrieveFile.getTextFile(fileId).toPromise();
+        this.description = JSON.parse(this.temp["message"]);
+        //
+
+        // Setting the booleans. with applications this time.
+        this.hasHeadline = true;
+        this.hasMarkdown = true;
+        this.isLoaded = true;
+        this.hasApplication = true;
+      } 
+      else {
+        this.description ="Stop forging stuff boi";
+      }
+
     } catch(err){
       console.error(err);
     }
--- src/app/metricselection/metricselection.component.ts
+++ src/app/metricselection/metricselection.component.ts
@@ -4,13 +4,6 @@ import { CommonModule } from '@angular/common';
 import { MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 
-import { VertRechComponent } from '../vert-rech/vert-rech.component';
-import { ConcurrencyComponent } from '../concurrency/concurrency.component';
-import { MaintenanceComponent } from '../maintenance/maintenance.component';
-import { DataintensiveComponent } from '../dataintensive/dataintensive.component';
-import { TestbarkeitComponent } from '../testbarkeit/testbarkeit.component';
-import { SecurityComponent } from '../security/security.component';
-import { FehlertoleranzComponent } from '../fehlertoleranz/fehlertoleranz.component';
 import { FunctionalConceptsExplainationComponent } from '../functional-concepts-explaination/functional-concepts-explaination.component';
 
 @Component({
@@ -21,15 +14,6 @@ import { FunctionalConceptsExplainationComponent } from '../functional-concepts-
   styleUrl: './metricselection.component.scss'
 })
 export class MetricselectionComponent {
-  listValues = ["Verteiltes Rechnen",
-    "Nebenläufigkeit",
-    "Wartbar- und Erweiterbarkeit",
-    "Datenintensiv",
-    "Sicherheitsanforderungen",
-    "Geringe Latenz",
-    "Fehlertoleranz",
-  ]
-
 
   constructor(@Inject(MatDialog) private dialog: MatDialog) { }
 
@@ -37,75 +21,12 @@ export class MetricselectionComponent {
   dialogConfig: MatDialogConfig = {
     maxHeight: '90vh',
     maxWidth: '80vh',
-};
+  };
 
-  // Dialog für Resusable Template. Erklärt die Oberbegriffe
-  openExplanationPF(headline: any, description: any){
-    event?.preventDefault();
-    this.dialogConfig.data={headline: headline, description: description};
-    this.dialog.open(FunctionalConceptsExplainationComponent, this.dialogConfig);
-  }
-  openExplanationHOF(headline: any, description: any){
-    event?.preventDefault();
-    this.dialogConfig.data={headline: headline, description: description};
-    this.dialog.open(FunctionalConceptsExplainationComponent, this.dialogConfig);
-  }
-  openExplanationImm(headline: any, description: any){
-    event?.preventDefault();
-    this.dialogConfig.data={headline: headline, description: description};
-    this.dialog.open(FunctionalConceptsExplainationComponent, this.dialogConfig);
-  }
-  openExplanationMon(headline: any, description: any){
-    event?.preventDefault();
-    this.dialogConfig.data={headline: headline, description: description};
-    this.dialog.open(FunctionalConceptsExplainationComponent, this.dialogConfig);
-  }
-  openExplanationPM(headline: any, description: any){
-    event?.preventDefault();
-    this.dialogConfig.data={headline: headline, description: description};
-    this.dialog.open(FunctionalConceptsExplainationComponent, this.dialogConfig);
-  }
-  openExplanationLE(headline: any, description: any){
-    event?.preventDefault();
-    this.dialogConfig.data={headline: headline, description: description};
-    this.dialog.open(FunctionalConceptsExplainationComponent, this.dialogConfig);
-  }
-
-
-
-  // Dialog für Verteiltes Rechnen
-  openVR() {
-    event?.preventDefault();
-    this.dialog.open(VertRechComponent, this.dialogConfig);
-  }
   // Dialog für Nebenläufigkeit
-  openConc() {
+  openDialog(headline: any) {
     event?.preventDefault();
-    this.dialog.open(ConcurrencyComponent, this.dialogConfig);
-  }
-  // Dialog für Wartbarkeit und Erweiterbarkeit
-  openWE() {
-    event?.preventDefault();
-    this.dialog.open(MaintenanceComponent, this.dialogConfig);
-  }
-  // Dialog für Datenintensiv
-  openD() {
-    event?.preventDefault();
-    this.dialog.open(DataintensiveComponent, this.dialogConfig);
-  }
-  // Dialog für Testbarkeit
-  openTest() {
-    event?.preventDefault();
-    this.dialog.open(TestbarkeitComponent, this.dialogConfig);
-  }
-  // Dialog für Sicherheit
-  openSec() {
-    event?.preventDefault();
-    this.dialog.open(SecurityComponent, this.dialogConfig);
-  }
-  // Dialog für Fehlertoleranz
-  openFault() {
-    event?.preventDefault();
-    this.dialog.open(FehlertoleranzComponent, this.dialogConfig);
+    this.dialogConfig.data={headline:headline};
+    this.dialog.open(FunctionalConceptsExplainationComponent, this.dialogConfig);
   }
 }
