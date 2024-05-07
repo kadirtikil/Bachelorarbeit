380535b2d8e50555392889c2263653c56880505f
Kadir Tikil <kadirtikil@gmail.com>
2024-05-07 13:21:36 +0200

    mp appl coming together

--- src/app/vert-rech/vert-rech.component.ts
+++ src/app/vert-rech/vert-rech.component.ts
@@ -17,20 +17,22 @@ import { DistributedSystemsService } from '../distributed-systems.service';
 export class VertRechComponent {
   dsForm = new FormGroup({
     pid: new FormControl("",[Validators.minLength(3), Validators.required, Validators.maxLength(9)]),
-    message: new FormControl('',[Validators.minLength(5), Validators.required, Validators.maxLength(20)])
+    message: new FormControl('',[Validators.minLength(5), Validators.required, Validators.maxLength(20)]),
+    requiredfor: new FormControl('', [Validators.minLength(1), Validators.maxLength(1)]),
   })
 
   // Array for messages. These will be send to the backend.
-  pidMessages: {pid: String; message: String;}[] = [];
+  pidMessages: {pid: String; message: String; requiredfor: String}[] = [];
 
   addMessages(): void {
     event?.preventDefault();
 
     const Pid = this.dsForm.value.pid
     const Message = this.dsForm.value.message;
+    const Requiredfor = this.dsForm.value.requiredfor;
 
-    if(Pid !== null && Pid !== undefined && Message !== null && Message !== undefined && this.pidMessages.length < 3){
-      this.pidMessages.push({pid: Pid?.toString(), message: Message?.toString()})
+    if(Pid !== null && Pid !== undefined && Message !== null && Message !== undefined && Requiredfor!==null && Requiredfor!==undefined  &&this.pidMessages.length < 3){
+      this.pidMessages.push({pid: Pid?.toString(), message: Message?.toString(), requiredfor: Requiredfor?.toString()})
     }
   }
 
